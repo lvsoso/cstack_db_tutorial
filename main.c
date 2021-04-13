c37c14f3e4a2fa0fb7b7d45300d32f2d73e8f650
lvsoso <wuqize5109@163.com>
2021-04-13 18:25:56 +0800

    add: cursor

--- main.c
+++ main.c
@@ -82,14 +82,24 @@ typedef struct
     Pager* pager;
 } Table;
 
+typedef struct
+{
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
+
 void print_row(Row* row){
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
 void serialize_row(Row* source, void* destination){
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    // memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    // memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 void deserialize_row(void *source, Row* destination){
@@ -129,12 +139,39 @@ void* get_page(Pager* pager, uint32_t page_num){
     return pager->pages[page_num];
 }
 
-void* row_slot(Table* table, uint32_t row_num){
+
+Cursor* table_start(Table* table){
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table){
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void* cursor_value(Cursor* cursor){
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = get_page(table->pager, page_num);
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    void* page = get_page(cursor->table->pager, page_num);
+    uint32_t row_offset = row_num / ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
-    return page + byte_offset;    
+    return page + byte_offset;
+}
+
+void cursor_advance(Cursor* cursor){
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows){
+        cursor->end_of_table = true;
+    }
 }
 
 
@@ -294,7 +331,7 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
     char* email = strtok(NULL, " ");
 
     if(id_string == NULL || username == NULL || email == NULL){
-        return PREPARE_SYNTAX_ERROR
+        return PREPARE_SYNTAX_ERROR;
     }
 
     int id = atoi(id_string);
@@ -337,20 +374,27 @@ ExecuteResult execute_insert(Statement* statement, Table* table){
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
 
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement* statement, Table* table){
+    Cursor* cursor = table_start(table);
+
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++)
+    while (!(cursor->end_of_table))
     {
-        deserialize_row(row_slot(table, i), &row);
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
+    
     return EXECUTE_SUCCESS;
 }
 

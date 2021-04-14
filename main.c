41968b42bd29a4aef0dd7ab05d4124dababdc158
lvsoso <wuqize5109@163.com>
2021-04-15 00:47:57 +0800

    add: binary search and duplicate keys

--- main.c
+++ main.c
@@ -15,7 +15,9 @@ typedef struct
 } InputBuffer;
 
 typedef enum { 
-    EXECUTE_SUCCESS, EXECUTE_TABLE_FULL 
+    EXECUTE_SUCCESS, 
+    EXECUTE_DUPLICATE_KEY,
+    EXECUTE_TABLE_FULL 
 } ExecuteResult;
 
 typedef enum {
@@ -178,6 +180,21 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
 }
 
 
+// 获取节点类型
+NodeType get_node_type(void* node){
+    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType)value;
+}
+
+
+// 设置节点类型
+void set_node_type(void* node, NodeType type){
+    uint8_t value = type;
+    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+
+
 // 输出 常量信息
 void print_constants(){
     printf("ROW_SIZE: %d\n", ROW_SIZE);
@@ -223,7 +240,12 @@ void deserialize_row(void *source, Row* destination){
 
 
 // 初始化叶子节点
-void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
+void initialize_leaf_node(void* node) { 
+    set_node_type(node, NODE_LEAF);
+    *leaf_node_num_cells(node) = 0; 
+}
+
+
 
 
 void* get_page(Pager* pager, uint32_t page_num){
@@ -275,18 +297,6 @@ Cursor* table_start(Table* table){
     return cursor;
 }
 
-Cursor* table_end(Table* table){
-    Cursor* cursor = malloc(sizeof(Cursor));
-    cursor->table = table;
-    cursor->page_num = table->root_page_num;
-
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
-    cursor->cell_num = num_cells;
-    cursor->end_of_table = true;
-
-    return cursor;
-}
 
 void* cursor_value(Cursor* cursor){
     uint32_t page_num = cursor->page_num;
@@ -531,21 +541,78 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value){
 }
 
 
-ExecuteResult execute_insert(Statement* statement, Table* table){
-    void* node = get_page(table->pager, table->root_page_num);
-    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)){
-        return EXECUTE_TABLE_FULL;
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key){
+    void* node = get_page(table->pager, page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->page_num = page_num;
+
+    // 二分搜索
+    uint32_t min_index = 0;
+    uint32_t one_past_max_index = num_cells;
+    while (one_past_max_index != min_index){
+        uint32_t index = (min_index + one_past_max_index) / 2;
+        uint32_t key_at_index = *leaf_node_key(node, index);
+        if (key == key_at_index){
+            cursor->cell_num = index;
+            return cursor;
+        }
+        if (key < key_at_index){
+            one_past_max_index = index;
+        }else{
+            min_index = index + 1;
+        }
     }
 
+    cursor->cell_num = min_index;
+    return cursor;
+}
+
+
+Cursor* table_find(Table* table, uint32_t key){
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+
+    if (get_node_type(root_node) == NODE_LEAF) {
+        return leaf_node_find(table, root_page_num, key);
+    }else{
+        printf("Need to implement searching an internal node\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+ExecuteResult execute_insert(Statement* statement, Table* table){
+    void* node = get_page(table->pager, table->root_page_num);
+
+    uint32_t num_cells = (*leaf_node_num_cells(node));
+    if (num_cells >= LEAF_NODE_MAX_CELLS){
+        return EXECUTE_TABLE_FULL;
+    }
+    
 
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
+
+    uint32_t key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+
+    if (cursor->cell_num < num_cells){
+        uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if (key_at_index == key_to_insert){
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
+
 
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
     free(cursor);
 
     return EXECUTE_SUCCESS;
 }
+
+
+
 
 ExecuteResult execute_select(Statement* statement, Table* table){
     Cursor* cursor = table_start(table);
@@ -624,6 +691,9 @@ int main(int argc, char *argv[])
         switch (execute_statement(&statement, table)){
             case (EXECUTE_SUCCESS):
                 printf("Executed.\n");
+                break;
+            case (EXECUTE_DUPLICATE_KEY):
+                printf("Error: Duplicate key.\n");
                 break;
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
